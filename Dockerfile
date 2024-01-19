26a562970b33eff9d201a0c02723660c072d4c83
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-01-19 03:28:00 +0000

    all issues were fixed

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@ FROM golang:1.21.3 as builder
 WORKDIR /app
 COPY go.mod go.sum ./
 RUN go mod download
-COPY . .
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bot .
+COPY *.go ./
+ENV CGO_ENABLED=0 GOOS=linux GOARCH=amd64
+RUN go build -a -installsuffix cgo -o yulia-lingo-backend
 
 FROM alpine:latest
 RUN apk --no-cache add ca-certificates
-WORKDIR /root/
-COPY --from=builder /app/bot .
-CMD ["./bot"]
+WORKDIR /app
+COPY --from=builder /app/yulia-lingo-backend .
+EXPOSE 8443
+CMD ["./yulia-lingo-backend"]
--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"os"
 )
 
-// album represents data about a record album.
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -13,7 +14,6 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-// albums slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
@@ -26,36 +26,38 @@ func main() {
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
-	router.Run("localhost:8080")
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		log.Fatal("Error. No PORT environment variable detected. Set the PORT environment variable.")
+	}
+
+	log.Printf("Starting server on port %s", port)
+	if err := router.Run(":" + port); err != nil {
+		log.Fatal("Failed to start server: ", err)
+	}
 }
 
-// getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-// postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
 	if err := c.BindJSON(&newAlbum); err != nil {
+		log.Printf("Error binding JSON: %v", err)
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid album data"})
 		return
 	}
 
-	// Add the new album to the slice.
 	albums = append(albums, newAlbum)
+	log.Printf("Added new album: %v", newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
 
-	// Loop through the list of albums, looking for
-	// an album whose ID value matches the parameter.
 	for _, a := range albums {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
