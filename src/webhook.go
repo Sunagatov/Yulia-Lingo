3a1d3738ed258cc3cc9095f9fe7688e1286ed5ca
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-01-20 00:03:20 +0000

    webhook.go was updated

--- src/webhook.go
+++ src/webhook.go
@@ -10,23 +10,26 @@ func setupWebhook(telegramBot *tgbotapi.BotAPI) {
 	webhookURL := os.Getenv("WEBHOOK_URL")
 	if webhookURL == "" {
 		log.Fatal("No WEBHOOK_URL provided in environment variables")
+		return
 	}
 	fullWebhookURL := webhookURL + "/" + telegramBot.Token
 
 	webhookConfig, webhookConfigError := tgbotapi.NewWebhook(fullWebhookURL)
 	if webhookConfigError != nil {
 		log.Fatalf("Failed to create webhook: %v", webhookConfigError)
+		return
 	}
 
 	_, webhookSetError := telegramBot.Request(webhookConfig)
 	if webhookSetError != nil {
-
 		log.Fatalf("Failed to set webhook: %v", webhookSetError)
+		return
 	}
 
 	info, webhookInfoError := telegramBot.GetWebhookInfo()
 	if webhookInfoError != nil {
 		log.Fatalf("Failed to get webhook info: %v", webhookInfoError)
+		return
 	}
 	if info.LastErrorDate != 0 {
 		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
