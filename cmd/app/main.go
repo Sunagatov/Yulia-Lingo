f18ede26bddcdb0791b832fb3ad9d0c50abec3f7
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-01-21 19:30:56 +0000

    refactoring

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"Yulia-Lingo/internal/server"
-	envFileLoader "Yulia-Lingo/pkg/common"
 	tg "Yulia-Lingo/pkg/telegram"
 )
 
 func main() {
-	envFileLoader.Load()
 	telegramBot := tg.CreateNewTelegramBot()
 	tg.SetupTelegramBotWebhook(telegramBot)
 	server.StartHTTPServer()
