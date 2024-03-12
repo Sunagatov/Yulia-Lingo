67b6020e22d0e72167a5dd416e4af752204f093e
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-03-12 21:07:19 +0000

    my_word_list_button_click_handler was added

--- internal/my_word_list/my_word_list_button_click_handler.go
+++ internal/my_word_list/my_word_list_button_click_handler.go
@@ -9,7 +9,6 @@ func HandleMyWordButtonClick(bot *tgbotapi.BotAPI, chatID int64) error {
 	messageText := "*Ваш список слов пуст*\n\n"
 	messageToUser := tgbotapi.NewMessage(chatID, messageText)
 	messageToUser.ParseMode = "Markdown"
-
 	_, err := bot.Send(&messageToUser)
 	if err != nil {
 		return fmt.Errorf("failed to send the message for 'IrregularVerbs' button to a user: %v", err)
